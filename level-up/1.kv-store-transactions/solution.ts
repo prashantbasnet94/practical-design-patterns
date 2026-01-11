2eac7a512a69a642f7ea89e829d56eefb5d1db51
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2026-01-11 11:47:48 -0600

    kv: add  layered transaction like undo and commit

--- level-up/1.kv-store-transactions/solution.ts
+++ level-up/1.kv-store-transactions/solution.ts
@@ -1,32 +1,48 @@
 
 export class KVStore {
+    stack: Map<string, string | null>[]
     constructor() {
-        // Initialize your data structure here
+
+        this.stack = [new Map<string, string| null>()]
     }
 
     set(key: string, value: string): void {
-        // TODO
+        this.stack[this.stack.length - 1].set(key, value)
     }
 
     get(key: string): string | null {
+        for (let i = this.stack.length - 1; -1 < i; i--){
+            let map = this.stack[i]
+              if (map.has(key)) {
+                 return map.get(key) || ''
+             }
+        }
         // TODO
         return null;
     }
 
     delete(key: string): void {
-        // TODO
+        let map = this.stack[this.stack.length -1]
+            map.set(key, null)
     }
 
     begin(): void {
-        // TODO
+       this.stack.push(new Map())
     }
 
     commit(): void {
-        // TODO
+        // take the top of the stack and commit to the prev verion of it
+        let topLayer = this.stack[ this.stack.length - 1 ]
+        let prevLayer = this.stack[ this.stack.length - 2 ]
+        if(!prevLayer) return
+        for (let [ k, v ] of topLayer) {
+            prevLayer.set(k, v)
+        }
+        this.stack.pop()
     }
 
     rollback(): void {
-        // TODO
+       1 < this.stack.length && this.stack.pop()
     }
 }
 
